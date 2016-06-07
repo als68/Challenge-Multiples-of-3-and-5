99e3363e370a88af5b9b79b78674cd057be44598
root <suhandyl@gmail.com>
2016-06-07 10:09:39 -1000

    Finished test

--- multiples.js
+++ multiples.js
@@ -8,5 +8,38 @@ exports.sumOfAMultiple = function( n ) {
 
   // do your work here
 
+/*var threeSum;
+var fiveSum;
+
+// this is how many even times 3 goes into n
+var flooredThree = Math.floor(multThree);
+console.log(flooredThree);
+
+for(i = 0; i < n; i++) {
+  var isMultThree = i % 3 === 0;
+  if (isMultThree === true) {
+      sum = sum + i;
+  }
+}
+
+// this is how many even times 5 goes into n
+var flooredFive = Math.floor(multFive);
+console.log(multFive);
+
+for(i = 0; i < n; i++) {
+  fiveSum = 5 * i;
+  sum += fiveSum;
+}
+
+  sum = threeSum + fiveSum;
+
+*/
+
+for(i = 0; i < n; i++) {
+  if (i % 3 === 0 || i % 5 === 0) {
+    sum += i;
+  }
+}
+
   return sum;
 };
